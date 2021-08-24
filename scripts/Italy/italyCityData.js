51c625e7c3cc3496501b54df82eae4dcb563f6e9
Devery Page <pagedevery@gmail.com>
2021-08-24 16:09:28 -0500

    added script data for italy

---
+++ scripts/Italy/italyCityData.js
@@ -0,0 +1,20 @@
+let italyCityData = [
+    {
+        name: "Rome",
+        population: "2,872,800",
+        description: "Full of modern and ancient culture",
+        link: "https://www.vatican.va/content/vatican/en.html"
+    },
+    {
+        name: "Vatican City",
+        population: "800",
+        description: "A hub for religious and spiritual energy",
+        link: "https://www.vatican.va/content/vatican/en.html"
+    },
+    {
+        name: "Florence",
+        population: "382,808",
+        description: "A city of beauty and romance",
+        link: "https://en.comune.fi.it/"
+    }
+]
