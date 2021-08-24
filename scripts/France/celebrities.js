abd320c1e2d25aa9892044d3ca2ed2048d8a080a
Matt F <trumpetman072392@gmail.com>
2021-08-24 16:32:26 -0500

    added france info and css

---
+++ scripts/France/celebrities.js
@@ -0,0 +1,25 @@
+export const getCitizens = () => {
+    return citizens
+}
+
+
+const citizens = [
+     {
+        name: "Gabrielle Coco Chanel",
+        occupation: "Fashion Designer",
+        description: "Fashion designer Coco Chanel is famous for her timeless designs, trademark suits and little black dresses",
+        link: "https://www.chanel.com/us/about-chanel/the-founder/"
+     },
+     {
+        name: "Napolean Bonaparte",
+        occupation: "French Military Leader",
+        description: "Napoleon Bonaparte was a French military general, the first emperor of France and one of the world's greatest military leaders.",
+        link: "https://www.history.com/topics/france/napoleon"
+     },
+     {
+        name: "Victor Hugo",
+        occupation: "Novelist & Poet",
+        description: "Regarded as one of the best-known and greatest French writers of all time, Victor Hugo wrote abundantly during his career that spanned over six decades.",
+        link: "https://www.biography.com/writer/victor-hugo"
+     }
+ ]
---
+++ scripts/France/data.js
@@ -0,0 +1,37 @@
+export const cityInfo = (cityObj) => {
+	return `<article class="card-style">
+				<div><img class="card-image" src="images/${cityObj.image}" /></div>
+				<h3>${cityObj.name}</h3>
+				<ul>
+					<li>Population: ${cityObj.population}</li>
+					<li>Description: ${cityObj.description} inches</li>
+					<li>Link: ${cityObj.link}</li>
+				</ul>
+        	</article>`
+}
+
+export const citizenInfo = (citizenObj) => {
+	return `<article class="card-style">
+				<div><img class="card-image" src="images/${citizenObj.image}" /></div>
+				<h3>${citizenObj.name}</h3>
+				<ul>
+					<li>${citizenObj.occupation}</li>
+					<li>Length: ${citizenObj.description} inches</li>
+					<li>Found: ${citizenObj.link}</li>
+				</ul>
+        	</article>`
+}
+
+export const landmarkInfo = (landObj) => {
+	return `<article class="card-style">
+				<div><img class="card-image" src="images/${landObj.image}" /></div>
+				<h3>${landObj.name}</h3>
+				<ul>
+					<li>${landObj.year}</li>
+					<li>Length: ${landObj.description} inches</li>
+					<li>Found: ${landObj.link}</li>
+				</ul>
+        	</article>`
+}
+
+
