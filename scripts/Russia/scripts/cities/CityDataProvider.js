d25f20d16d57c72673bb8244d3b0b9d7b9ca2614
JamaCodes <jama.avwork@gmail.com>
2021-08-25 10:16:25 -0500

    russia working javascript

---
+++ scripts/Russia/scripts/cities/CityDataProvider.js
@@ -0,0 +1,27 @@
+export const getCities = () => {
+    return cities
+}
+const cities = [
+    {
+        name: "Moscow",
+        population: "11,920,000",
+        description: "First documented in 1147, Moscow grew to become a prosperous and powerful city that served as the capital of the Grand Duchy that bears its namesake. When the Grand Duchy of Moscow evolved into the Tsardom of Russia, Moscow still remained as the political and economic center for most of the Tsardom's history. When the Tsardom was reformed into the Russian Empire",
+        link: "https://en.wikipedia.org/wiki/Moscow",
+        linkDescription: "Moscow Wiki"
+    },
+    {
+        name: "St. Petersburg",
+        population: "4,900,000",
+        description: "Saint Petersburg is known as the 'Cultural Capital of Russia',and received over 15 million tourists in 2018.[14][15] It is considered an important economic, scientific, cultural, and tourism centre of Russia and Europe.",
+        link: "https://en.wikipedia.org/wiki/Saint_Petersburg",
+        linkDescription: "St. Petersburg Wiki"
+    },
+    
+    {
+        name: "Chernobyl ",
+        population: "500",
+        description: "On 26 April 1986, one of the reactors at the Chernobyl Nuclear Power Plant exploded after unsanctioned experiments on the reactor by plant operators were done improperly.",
+        link: "https://en.wikipedia.org/wiki/Chernobyl",
+        linkDescription: "Chernobyl Wiki"
+    }
+]
---
+++ scripts/Russia/scripts/landmarks/LandmarkDataProvider.js
@@ -0,0 +1,29 @@
+
+export const getLandmarks = () => {
+    return landmarks
+}
+
+
+const landmarks = [
+    {
+        name: "Moscow Kremlin",
+        year: "1495",
+        description: " It is the best known of the kremlins (Russian citadels), and includes five palaces, four cathedrals, and the enclosing Kremlin Wall with Kremlin towers. In addition, within this complex is the Grand Kremlin Palace that was formerly the Tsar's Moscow residence.",
+        link: "https://en.wikipedia.org/wiki/Moscow_Kremlin",
+        linkDescription: "Moscow Kremlin Wiki"
+    },
+    {
+        name: "St. Petersburg",
+        year: "10 bc",
+        description: "Saint Petersburg is known as the 'Cultural Capital of Russia',and received over 15 million tourists in 2018.[14][15] It is considered an important economic, scientific, cultural, and tourism centre of Russia and Europe.",
+        link: "https://en.wikipedia.org/wiki/Saint_Petersburg",
+        linkDescription: "St. Petersburg Wiki"
+    },
+    {
+        name: "Chernobyl Exclusion Zone",
+        year: "1986",
+        description: "Officially designated exclusion zone around the site of the Chernobyl nuclear reactor disaster.Established by the Soviet Armed Forces soon after the 1986 disaster.",
+        link: "https://en.wikipedia.org/wiki/Saint_Petersburg",
+        linkDescription: "St. Petersburg",
+    }
+]
