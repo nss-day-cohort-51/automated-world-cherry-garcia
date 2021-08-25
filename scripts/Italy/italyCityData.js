04bf696fdf489e2aeec8719c27a6c18c77295726
Devery Page <pagedevery@gmail.com>
2021-08-25 11:30:30 -0500

    italys images

--- scripts/Italy/italyCityData.js
+++ scripts/Italy/italyCityData.js
@@ -4,18 +4,21 @@ export const getItalyCityData = () => {
 
 const italyCityData = [
     {
+        image: "../../images/italy-images/rome.jpg",
         name: "Rome",
         population: "2,872,800",
         description: "Full of modern and ancient culture",
         link: "https://www.vatican.va/content/vatican/en.html"
     },
     {
+        image: "../../images/italy-images/vaticancity.jpg",
         name: "Vatican City",
         population: "800",
         description: "A hub for religious and spiritual energy",
         link: "https://www.vatican.va/content/vatican/en.html"
     },
     {
+        image: "../../images/italy-images/florence.jpg",
         name: "Florence",
         population: "382,808",
         description: "A city of beauty and romance",
--- scripts/Italy/italyLandMarkData.js
+++ scripts/Italy/italyLandMarkData.js
@@ -2,20 +2,23 @@ export const getItalyLandMarkData = () => {
     return italyLandMarkData;
 }
 
-const italyLandMarkData = [
+let italyLandMarkData = [
     {
+        image: "../../images/italy-images/vatican.jpg",
         name: "Vatican",
         year: "852",
         description: "A religous center",
         link: "https://www.vatican.va/content/vatican/en.html"
     },
     {
+        image: "../../images/italy-images/colosseum.jpg",
         name: "Roman Colosseum",
         year: "80 A.D",
         description: "A beautiful historic piece of ancient arcitecture",
         link: "https://www.thecolosseum.org/visit/"
     },
     {
+        image: "../../images/italy-images/sistine.jpg",
         name: "Sistine Chappel",
         year: "1482",
         description: "A religous piece of art surrounded by like-minded followers",
