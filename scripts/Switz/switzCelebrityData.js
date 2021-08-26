d796edc231da89f9f7b4a3dd40edac76d84147f0
Brady Williams <williams.cole29@yahoo.com>
2021-08-26 13:08:55 -0500

    Added alt text and target _blank

--- scripts/Switz/switzCelebrityData.js
+++ scripts/Switz/switzCelebrityData.js
@@ -9,21 +9,24 @@ const switzCelebData = [
         name: "Roger Federer",
         occupation: "Tennis Player",
         description: "Roger Federer holds several ATP records and is considered to be one of the greatest tennis players of all time.",
-        link: "https://www.rogerfederer.com/"
+        link: "https://www.rogerfederer.com/",
+        altText: "Image of Roger Federer"
     },
     {
         image: "../images/switz-images/albert-einstein.jpg",
         name: "Albert Einstein",
         occupation: "Physicist",
         description: "Albert Einstein was a German/Swiss mathematician and physicist who developed the special and general theories of relativity.",
-        link: "https://en.wikipedia.org/wiki/Albert_Einstein"
+        link: "https://en.wikipedia.org/wiki/Albert_Einstein",
+        altText: "Image of Albert Einstein"
     },
     {
         image: "../images/switz-images/jean-rousseau.jpg",
         name: "Jean-Jaques Rousseau",
         occupation: "Philosopher",
         description: "Jean-Jaques Rousseau was a Genevan philosopher, writer, and composer.",
-        link: "https://en.wikipedia.org/wiki/Jean-Jacques_Rousseau"
+        link: "https://en.wikipedia.org/wiki/Jean-Jacques_Rousseau",
+        altText: "Image of Jean-Jaques Rousseau"
     }
 
 ]
--- scripts/Switz/switzLandMarkData.js
+++ scripts/Switz/switzLandMarkData.js
@@ -8,21 +8,24 @@ const switzMarkData = [
         name: "Lake Geneva",
         year: "58 B.C.E.",
         description: "Lake Geneva is a deep lake on the north side of the Alps, shared between Switzerland and France. It is one of the largest lakes in Western Europe and the largest on the course of the Rhône.",
-        link: "https://en.wikipedia.org/wiki/Lake_Geneva"
+        link: "https://en.wikipedia.org/wiki/Lake_Geneva",
+        altText: "Image of Lake Geneva"
     },
     {
         image: "../images/switz-images/matterhorn.jpg",
         name: "The Matterhorn",
         year: "1865",
         description: " The legend of the Matterhorn was born in 1865 when the peak was conquered for the first time and news of the event quickly travelled around the world.",
-        link: "https://en.wikipedia.org/wiki/Geneva"
+        link: "https://en.wikipedia.org/wiki/Matterhorn",
+        altText: "Image of Matterhorn"
     },
     {
         image: "../images/switz-images/castello.jpg",
         name: "Castello Di Montebello",
         year: "1313",
         description: "Castello Di Montebello is located to the east of the town center. It was built before 1313 for the pro-Imperial Rusca family, who occupied the castle following the Visconti victory and occupation of Castelgrande.",
-        link: "https://en.wikipedia.org/wiki/Castles_of_Bellinzona#Montebello"
+        link: "https://en.wikipedia.org/wiki/Castles_of_Bellinzona#Montebello",
+        altText: "Image of Castello di Montebello"
     }
 
 ]
